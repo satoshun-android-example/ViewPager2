bfa909300c9da3629cc13d4dabbf13f14dfa1ab2
SatoShun <shun.sato1@gmail.com>
2019-10-13 04:28:52 +0000

    remove colors

--- app/src/main/java/com/github/satoshun/example/infinite/InfiniteAdapter.kt
+++ app/src/main/java/com/github/satoshun/example/infinite/InfiniteAdapter.kt
@@ -1,19 +1,9 @@
 package com.github.satoshun.example.infinite
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.github.satoshun.example.databinding.InfiniteItemBinding
-
-private val colors = listOf(
-  Color.BLACK,
-  Color.RED,
-  Color.GREEN,
-  Color.BLUE,
-  Color.GRAY,
-  Color.YELLOW
-)
 
 class InfiniteAdapter(
   private val data: List<InfiniteData>
@@ -33,7 +23,6 @@ class InfiniteAdapter(
   override fun onBindViewHolder(holder: InfiniteViewHolder, position: Int) {
     val data = data[position % data.size]
     holder.binding.title.text = data.title
-    holder.binding.container.setBackgroundColor(colors[position % colors.size])
   }
 }
 
