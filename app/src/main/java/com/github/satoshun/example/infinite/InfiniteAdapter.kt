c6cb0faf29d15997ffbfc5740492d098cca33604
SatoShun <shun.sato1@gmail.com>
2019-10-13 04:23:40 +0000

    colors

--- app/src/main/java/com/github/satoshun/example/infinite/InfiniteAdapter.kt
+++ app/src/main/java/com/github/satoshun/example/infinite/InfiniteAdapter.kt
@@ -1,9 +1,19 @@
 package com.github.satoshun.example.infinite
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.github.satoshun.example.databinding.InfiniteItemBinding
+
+private val colors = listOf(
+  Color.BLACK,
+  Color.RED,
+  Color.GREEN,
+  Color.BLUE,
+  Color.GRAY,
+  Color.YELLOW
+)
 
 class InfiniteAdapter(
   private val data: List<InfiniteData>
@@ -11,19 +21,19 @@ class InfiniteAdapter(
 
   override fun getItemCount(): Int = Int.MAX_VALUE
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InfiniteViewHolder {
-    return InfiniteViewHolder(
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InfiniteViewHolder =
+    InfiniteViewHolder(
       InfiniteItemBinding.inflate(
         LayoutInflater.from(parent.context),
         parent,
         false
       )
     )
-  }
 
   override fun onBindViewHolder(holder: InfiniteViewHolder, position: Int) {
     val data = data[position % data.size]
     holder.binding.title.text = data.title
+    holder.binding.container.setBackgroundColor(colors[position % colors.size])
   }
 }
 
